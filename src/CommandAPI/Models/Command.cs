b01a95fd1bd95ce51ea48519c573d06ecbc1a907
Mike Kargin <gray_cloud@mail.ru>
2021-04-07 12:43:04 +0300

    Added Command Model to AI Progect _

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,23 @@
+using System.ComponentsModel.DataAnnotation;
+
+
+
+namespace CommandAPI.Models
+{
+    public class Command
+    {
+        [Key] //primary key
+        [Required] //not be null
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
